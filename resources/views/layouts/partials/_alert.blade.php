e5e3b89223984ba2cf689fef8f7519ab3aaa45ec
adit <adityamuhamamdputra@gmail.com>
2019-07-30 11:02:42 +0700

    test

---
+++ resources/views/layouts/partials/_alert.blade.php
@@ -0,0 +1,29 @@
+
+@if(session('success'))
+<div class="alert alert-success alert-dismissible" role="alert">
+	<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	{{session('success')}}
+</div>
+@endif
+
+@if(session('info'))
+<div class="alert alert-info alert-dismissible" role="alert">
+	<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	{{session('info')}}
+</div>
+@endif
+
+@if(session('danger'))
+<div class="alert alert-danger alert-dismissible" role="alert">
+	<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	{{session('danger')}}
+</div>
+@endif
+
+<script type="text/javascript">
+	window.setTimeout(function() {
+    $(".alert").fadeTo(500, 0).slideUp(500, function(){
+        $(this).remove(); 
+    });
+}, 3000);
+</script>
